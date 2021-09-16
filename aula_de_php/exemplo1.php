ae212a9dc9e40080af3fc968ad307815a1a478fa
Marcio Alessandro Dos Santos <marciobroficial@outlook.com>
2021-09-16 11:27:41 -0300

    Primeiro ex. em PHP

---
+++ aula_de_php/exemplo1.php
@@ -0,0 +1,23 @@
+<!-- <?php
+echo "oi";
+?> -->
+
+<!DOCTYPE>
+<html>
+<head>
+    <title> Ptimeiro Exemplo PHP </title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<div> 
+    <?php
+        for($i = 1; $i <= 6; $i++){
+            echo ("<h$i>oi<h$i>");
+        } 
+    ?>    
+
+<div>
+</body>
+</html>       
+
+
